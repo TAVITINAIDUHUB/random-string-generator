aadc201a934ac14209fe86db751f332d94c7ca31
taviti Naidu <tavitinaidu@tavitis-MacBook-Air.local>
2025-07-21 09:40:39 +0530

    Your commit message here

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,7 @@
 function About() {
     return (
       <div className="page">
-        <h2>This is the About Page  naidu
+        <h2>This is the About Page  
              </h2>
       </div>
     );
